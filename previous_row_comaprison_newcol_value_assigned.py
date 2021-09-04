7eb367bc97622bd6aa9ca08f95a727d356548a5e
DS Deepanshu Samdani <39222636+deepanshusamdani@users.noreply.github.com>
2021-09-04 13:52:28 +0530

    Create previous_row_comaprison_newcol_value_assigned.py

---
+++ previous_row_comaprison_newcol_value_assigned.py
@@ -0,0 +1,43 @@
+#previous_row_comaprison_newcol_value_assigned
+"""
+********* INPUT **************
+Name	Age	subject	  marks	ID
+Ankit	25	science	  80	   1
+Ankit	25	social	  70	   1
+Anuj	26	maths	    90	   2
+Anuj	26	english	  80	   2
+Anil	27	sanskrit	70	   3
+Ram	  28	maths	    60	   4
+shyam	29	hindi	    70	   5
+mohan	30	english	  80	   6
+
+********* OUTPUT **************
+Name	Age	subject	 marks	ID	new
+Ankit	25	science	  80	  1	  80
+Ankit	25	social	  70	  1	  150
+Anuj	26	maths	    90	  2	  90
+Anuj	26	english	  80	  2	  170
+Anil	27	sanskrit	70	  3	  70
+Ram	  28	maths	    60	  4	  60
+shyam	29	hindi	    70	  5	  70
+mohan	30	english	  80	  6	  80
+"""
+# CODE: --------------------------------------------------------------------------------------------------
+import pandas as pd
+data = pd.read_excel("/home/deepu/Downloads/prob.xlsx")
+df = pd.DataFrame(data)
+a = df['ID']
+b = df['marks']
+df['new'] = df['marks']
+c = df['new']
+aLen = len(a)
+for i, j in enumerate(a):
+    if i != (aLen-1):
+        if a[i+1] == a[i]:
+            c[i] = b[i]
+            c[i+1] = b[i]+b[i+1]
+        else:
+            c[i+1] = b[i+1]
+    else:
+        pass
+print(df)
